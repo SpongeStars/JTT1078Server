ecbbc9db2e411cbe624b8e1f7cdcb53789c0d182
chris <chrisliu.top@gmail.com>
2020-04-10 14:45:41 +0800

    support eureka server

--- src/main/java/com/tsingtech/jtt1078/JTT1078Application.java
+++ src/main/java/com/tsingtech/jtt1078/JTT1078Application.java
@@ -2,11 +2,13 @@ package com.tsingtech.jtt1078;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 白茶清欢无别事，我在等风也等你
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JTT1078Application {
 
 	public static void main(String[] args) {
